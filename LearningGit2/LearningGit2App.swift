b1b50fcde16cf51afa403a1f4cc68f83514b583e
Sainath <sainathbamen3697@gmail.com>
2026-04-13 10:08:16 +0530

    Initial Commit

---
+++ LearningGit2/LearningGit2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGit2App.swift
+//  LearningGit2
+//
+//  Created by Sainath bamen on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningGit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
