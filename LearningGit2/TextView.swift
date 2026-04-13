e81d4871c1c6ce39021b5418388afebd9734025c
Sainath <sainathbamen3697@gmail.com>
2026-04-13 15:49:49 +0530

    Create TextView basic structure

---
+++ LearningGit2/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  LearningGit2
+//
+//  Created by Sainath bamen on 13/04/26.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text("MyTextView")
+    }
+}
+
+#Preview {
+    TextView()
+}
